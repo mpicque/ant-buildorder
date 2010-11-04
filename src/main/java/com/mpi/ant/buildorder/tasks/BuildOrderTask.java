714291211787dcd61f6bbf1c801d9341932aff65
neomaxp <neomaxp@1d0d1878-99c6-b1d5-b862-aa4da8c98f50>
2010-11-04 10:03:48 +0000

    Separate log & depency tree managment

--- src/main/java/com/mpi/ant/buildorder/tasks/BuildOrderTask.java
+++ src/main/java/com/mpi/ant/buildorder/tasks/BuildOrderTask.java
@@ -11,6 +11,8 @@
  */
 package com.mpi.ant.buildorder.tasks;
 
+import java.util.List;
+
 import org.apache.tools.ant.BuildException;
 
 import com.mpi.ant.buildorder.tasks.tree.DependencyTree;
@@ -44,21 +46,36 @@ public class BuildOrderTask extends AbstractDependencyLoaderTask {
 
         DependencyTree tree = loadDependencyTree();
         DependencyTree result = DependencyTreeResolver.resolve(tree);
-        String dependencyList = result.toDependencyList(separator);
-        log("Computed build order : " + dependencyList);
-        getProject().setProperty(property, dependencyList);
+        List<String> dependencyList = result.toDependencyList();
+        String dependenciesStr = concat(dependencyList, separator);
+        getProject().setProperty(property, dependenciesStr);
     }
 
     @Override
     protected void checkProperties() throws BuildException {
         super.checkProperties();
-        
+
         if (property == null || property.trim().length() == 0) {
             throw new BuildException("attribute property must be set");
         }
-        
+
         if (separator == null || separator.length() == 0) {
             throw new BuildException("attribute separator must be set");
         }
+    }
+
+    private String concat(List<String> dependencies, String separator) {
+        StringBuilder sb = new StringBuilder();
+
+        log("Computed build order : ");
+        for (int i = 0; i < dependencies.size(); i++) {
+            log("\t" + (i + 1) + "- " + dependencies.get(i));
+            sb.append(dependencies.get(i));
+            if (i != dependencies.size() - 1) {
+                sb.append(separator);
+            }
+        }
+
+        return sb.toString();
     }
 }
