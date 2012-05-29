afd6c7d2467590a8cb06319c87b61e8a962bb984
maxime.picque@gmail.com <maxime.picque@gmail.com@1d0d1878-99c6-b1d5-b862-aa4da8c98f50>
2012-05-29 08:22:35 +0000

    deleting obsolete comments

--- trunk/src/main/java/com/mpi/ant/buildorder/tasks/AbstractDependencyLoaderTask.java
+++ trunk/src/main/java/com/mpi/ant/buildorder/tasks/AbstractDependencyLoaderTask.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2001-2010 Group JCDecaux.
- * 17 rue Soyer, 92523 Neuilly Cedex, France.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Group JCDecaux ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you
- * entered into with Group JCDecaux.
- */
 package com.mpi.ant.buildorder.tasks;
 
 import java.io.File;
